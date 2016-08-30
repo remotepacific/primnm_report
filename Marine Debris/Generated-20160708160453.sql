255870152e1a5ece49561a69f69bfe505013f2e0
remotepacific <brencorals@gmail.com>
2016-08-29 15:27:51 -1000

    Marine debris
    Data folder from the T drive

---
+++ Marine Debris/Generated-20160708160453.sql
@@ -0,0 +1,5 @@
+--------------------------------------------------------
+--  File created - Friday-July-08-2016   
+--------------------------------------------------------
+@T:\CRED Publications\MonitoringReport\PacificRemoteIslands\_MonitoringReport\_Data\ESI\V0_BENT_TDS.sql
+--@T:\CRED Publications\MonitoringReport\PacificRemoteIslands\_MonitoringReport\_Data\ESI\V0_BENT_TDS_DATA_VIEW.xlsx
